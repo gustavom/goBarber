8f7b5f5d9cdb086cf34fcc2b8d8b95f4ec9911c0
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-06-27 00:02:20 -0300

    user register with controller

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,18 @@
 import { Router } from 'express';
-import User from './app/models/user';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
-
-routes.get('/', async (req, res) => {
+// rota para teste
+/* routes.get('/', async (req, res) => {
   const user = await User.create({
     name: 'Gustavo Martusewicz',
     email: 'gustavo_crj@hotmail.com',
     password_hash: '123456',
   });
   res.json(user);
-});
+}); */
+
+routes.post('/users', UserController.store);
 
 export default routes;
