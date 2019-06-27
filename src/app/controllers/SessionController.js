619db5796e3bb34f16b6a35b073256e940e38e64
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-06-27 12:36:22 -0300

    verify email exists

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,7 +2,13 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User';
 
 class SessionController {
-  async store(req, res) {}
+  async store(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      res.status(400).json({ error: 'User not found.' });
+    }
+  }
 }
 
 export default new SessionController();
