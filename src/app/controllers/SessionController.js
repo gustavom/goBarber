336a51504528373c3f26978bdfe0ad3efe872cb3
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-06-27 14:48:16 -0300

    indent and change error 400 for 401

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,17 +1,23 @@
 import jwt from 'jsonwebtoken';
+
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
+
     const user = await User.findOne({ where: { email } });
+
     if (!user) {
       res.status(400).json({ error: 'User not found.' });
     }
+
     if (!(await user.checkPassword(password))) {
-      res.status(400).json({ error: 'Password does not match.' });
+      res.status(401).json({ error: 'Password does not match.' });
     }
+
     const { id, name } = user;
+
     return res.json({
       user: {
         id,
