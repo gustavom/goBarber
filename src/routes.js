4ce22c7217af54b68502d21cdaac0724c6bd2fcc
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-06-27 14:47:58 -0300

    add sessions route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 // rota para teste
@@ -14,5 +15,6 @@ const routes = new Router();
 }); */
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
