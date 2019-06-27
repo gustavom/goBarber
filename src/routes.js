69c40687290253c536b36e5bed4ec863cfe4db42
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-06-26 23:48:18 -0300

    create user model, and register

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/user';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Gustavo Martusewicz',
+    email: 'gustavo_crj@hotmail.com',
+    password_hash: '123456',
+  });
+  res.json(user);
+});
 
 export default routes;
