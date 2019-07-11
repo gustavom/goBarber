978e9116f1dcde8175bfae7678429a0e2c2c7a92
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-11 19:05:26 -0300

    redis config

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,9 @@ import File from '../models/File';
 import Appointment from '../models/Appointments';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+// import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -142,18 +144,7 @@ class AppointmentController {
     appointment.canceled_at = new Date();
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento Cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
-    });
+    await Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
---
+++ src/config/redis.js
@@ -0,0 +1,5 @@
+export default {
+  host: 'redis-14725.c14.us-east-1-3.ec2.cloud.redislabs.com',
+  port: 14725,
+  password: 123456,
+};
